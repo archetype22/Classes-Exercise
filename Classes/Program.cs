0d5f1829c4a7dacb7ed6048d95648b20f9d48d91
nathan stephens <nstephens1234567@gmail.com>
2022-12-07 14:25:33 -0600

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Make = "Chevrolet";
+            car1.Model = "Apache";
+            car1.Year = 1959;
+
+            Console.WriteLine($"The {car1.Year} {car1.Make} {car1.Model} is an iconic pickup truck! Put it on 36's and you're the talk of the town!");
         }
     }
 }
